4f79e71200d6c14a69686580af6c2ddd1a7bccaa
brona <brona@192.168.1.7>
2017-09-15 22:52:13 +0100

    course/module updates

--- assignment/src/main/java/software_eng/assignment/Course.java
+++ assignment/src/main/java/software_eng/assignment/Course.java
@@ -8,27 +8,45 @@ public class Course {
 	private List<Module> modules;
 	private DateTime startdate, endDate;
 	
+	public Course() {
+		
+	}
+	
+	public Course(String courseName, List<Module> modules, DateTime startDate, DateTime endDate) {
+		setCourseName(courseName);
+		setModules(modules);
+		setStartdate(startDate);
+		setEndDate(endDate);
+	}
+	
 	public String getCourseName() {
 		return courseName;
 	}
+	
 	public void setCourseName(String courseName) {
 		this.courseName = courseName;
 	}
+	
 	public List<Module> getModules() {
 		return modules;
 	}
+	
 	public void setModules(List<Module> modules) {
 		this.modules = modules;
 	}
+	
 	public DateTime getStartdate() {
 		return startdate;
 	}
+	
 	public void setStartdate(DateTime startdate) {
 		this.startdate = startdate;
 	}
+	
 	public DateTime getEndDate() {
 		return endDate;
 	}
+	
 	public void setEndDate(DateTime endDate) {
 		this.endDate = endDate;
 	}
--- assignment/src/main/java/software_eng/assignment/Module.java
+++ assignment/src/main/java/software_eng/assignment/Module.java
@@ -6,21 +6,36 @@ public class Module {
 	private String moduleName, moduleID;
 	private List<Student> studentList;
 	
+	public Module() {
+		
+	}
+	
+	public Module(String moduleName, String moduleID, List<Student> studentList) {
+		setModuleName(moduleName);
+		setModuleID(moduleID);
+		setStudentList(studentList);
+	}
+	
 	public String getModuleName() {
 		return moduleName;
 	}
+	
 	public void setModuleName(String moduleName) {
 		this.moduleName = moduleName;
 	}
+	
 	public String getModuleID() {
 		return moduleID;
 	}
+	
 	public void setModuleID(String moduleID) {
 		this.moduleID = moduleID;
 	}
+	
 	public List<Student> getStudentList() {
 		return studentList;
 	}
+	
 	public void setStudentList(List<Student> studentList) {
 		this.studentList = studentList;
 	}
